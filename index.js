2996ec6570b268ae85aa6848937c3fe672c63e4e
carmenjoekalda <cjoekalda@gmail.com>
2024-04-10 09:38:45 +0300

    input form

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
+const path = require('path')
+
+app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'html')
+
+app.use(express.static('views'))
 
 app.get('/', (req, res) => {
-    res.send('success')
+    res.render('./index.html')
   })
 
 app.listen(3040, () => {
